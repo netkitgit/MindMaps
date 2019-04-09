0616be12868b37609ce01db2a8442fa7f6889fdd
User Demogit <demo@demogit.com>
2019-04-09 15:44:25 +0300

    init commit

---
+++ webapp-testing.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1554281816485" ID="ID_1257266326" MODIFIED="1554281849351" TEXT="&#x422;&#x435;&#x441;&#x442;&#x438;&#x440;&#x43e;&#x432;&#x430;&#x43d;&#x438;&#x435; &#x412;&#x435;&#x431;-&#x43f;&#x440;&#x438;&#x43b;&#x43e;&#x436;&#x435;&#x43d;&#x438;&#x439;"/>
+</map>
