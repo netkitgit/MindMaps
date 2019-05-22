dab642aa3357dd56a14b6e931ed13f293eb7ff1f
netkit@domain.com <demo@demogit.com>
2019-05-22 10:55:27 +0300

    add new mm, changed test-disaign

---
+++ selenium-webdriver-java-cucumber-bdd.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1557910988401" ID="ID_940687405" MODIFIED="1557911013174" TEXT="selenium-webdriver-java-cucumber-bdd"/>
+</map>
