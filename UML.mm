66765e698f904e85877afb9d13e90a4105789a0d
netkitgit <48982244+netkitgit@users.noreply.github.com>
2019-08-03 14:32:28 +0300

    Create UML.mm
    add uml mm

---
+++ UML.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1564831889783" ID="ID_1315946092" MODIFIED="1564831899747" TEXT="UML"/>
+</map>
